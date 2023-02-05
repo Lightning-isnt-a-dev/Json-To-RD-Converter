49a03b0f5b3b05f69df2bd967ed4e7349f2f697e
Lightning <112514086+Lightning-isnt-a-dev@users.noreply.github.com>
2023-02-05 14:28:52 +0200

    Update json to rd.py

--- json to rd.py
+++ json to rd.py
@@ -6,23 +6,27 @@ import os, sys
 jsonpath = input("Please input the path of your json plugin: ")
 
 if os.path.exists(jsonpath):
-    x = json.load(open(jsonpath))
+    try :
+        x = json.load(open(jsonpath, encoding='utf-8'))
+    except UnicodeDecodeError:
+        sys.exit('Oops! Something happened. Report the error on github or to me on discord: Lightning#2538')
 else:
     print("Theres something wrong with your path. Try again.")
 
-#checksigns = ["Name", "default_name"]
-
-#for sign in x:
-#    if sign == "Name":
-#        sys.exit()
-#    elif sign == "default_name":
-#        print("Detected")
-#        sys.exit
-
-signs = [("Name","Name"), ("Swapicon","Icon"), ("Message", "Author")]
+while True:
+    try:
+        x["Name"]
+    except KeyError:
+        signs = [("swapped_name","Name"), ("default_icon","Icon"), ("messages", "Author")]
+        difjson = True
+        break
+    else:
+        signs = [("Name","Name"), ("Swapicon","Icon"), ("Message", "Author")]
+        difjson = False
+        break
 
 print("Writing signs...")
-wfile = open(os.path.basename(jsonpath).split(".")[0] + ".rd", "w")
+wfile = open(os.path.basename(jsonpath).split(".")[0] + ".rd", "w", encoding="utf-8")
 wfile.write("#file plugin\n")
 for sign, sign_name in signs:
     signing = ('sign: "%s", "%s"\n' % (sign_name, x[sign]))
@@ -32,7 +36,14 @@ print("Wrote signs!")
 swapar = 0
 print("Writing imports and swaps...")
 
-for asset in x["Assets"]:
+difjsonassets = ""
+
+if difjson == True:
+    difjsonassets = x["swaps"]
+elif difjson == False:
+    difjsonassets = x["Assets"]
+    
+for asset in difjsonassets:
     try:
         AssetPathTo = asset["AssetPathTo"]
     except KeyError:
@@ -60,47 +71,64 @@ for asset in x["Assets"]:
         fromar = "from_ar = import \"" + AssetPath + "\" \n"
         wfile.write(fromar)
 
-    swaps = asset["Swaps"]
-    swaps_sorted = sorted(swaps, key=lambda d: len(d['replace']), reverse=True)
-    for swap in swaps_sorted:
-        searchstring = swap["search"]
-        replacestring = swap["replace"]
-        if replacestring == "/":
-            wstring = "/"
+    try:
+        swaps = asset["Swaps"]
+    except KeyError:
+        swaps = asset["swaps"]
+    else:
+        swaps = asset["Swaps"]
+    if difjson == True:
+        print(x["swaps"])
+    
+    elif difjson == False:
+        swaps = asset["Swaps"]
+        try:
+            swaps_sorted = sorted(swaps, key=lambda d: len(d['replace']), reverse=True)
+        except TypeError:
+            swapsarenull = True
         else:
-            wstring = os.path.basename(replacestring).split(".")[1]
-            wstringlong = os.path.basename(replacestring).split(".")[0]
-        sstring = os.path.basename(searchstring).split(".")[1]
-        sstringlong = os.path.basename(searchstring).split(".")[0]
+            swapsarenull = False
+        if swapsarenull == True:
+            break
+        elif swapsarenull == False:
+            for swap in swaps_sorted:
+                searchstring = swap["search"]
+                replacestring = swap["replace"]
+                if replacestring == "/":
+                    wstring = "/"
+                else:
+                    wstring = os.path.basename(replacestring).split(".")[1]
+                    wstringlong = os.path.basename(replacestring).split(".")[0]
+                sstring = os.path.basename(searchstring).split(".")[1]
+                sstringlong = os.path.basename(searchstring).split(".")[0]
 
-        dirandstring = os.path.dirname(searchstring) + "/" + sstringlong
-        dirandwrite = os.path.dirname(replacestring) + "/" + wstringlong
+                dirandstring = os.path.dirname(searchstring) + "/" + sstringlong
+                dirandwrite = os.path.dirname(replacestring) + "/" + wstringlong
 
-        if pathtoexists == True:
-            wfile.write("to_ar.Seek(\"" + dirandstring + "\")" + "\n")
-            wfile.write("to_ar.Write<string>(\"" + dirandwrite + "\")" + "\n\n")
+                if pathtoexists == True:
+                    wfile.write("to_ar.Seek(\"" + dirandstring + "\")" + "\n")
+                    wfile.write("to_ar.Write<string>(\"" + dirandwrite + "\")" + "\n\n")
 
-            wfile.write("to_ar.Seek(\"" + sstring + "\")" + "\n")
-            wfile.write("to_ar.Write<string>(\"" + wstring + "\")" + "\n\n")
-        elif pathtoexists == False:
-            wfile.write("from_ar.Seek(\"" + dirandstring + "\")" + "\n")
-            wfile.write("from_ar.Write<string>(\"" + dirandwrite + "\")" + "\n\n")
+                    wfile.write("to_ar.Seek(\"" + sstring + "\")" + "\n")
+                    wfile.write("to_ar.Write<string>(\"" + wstring + "\")" + "\n\n")
+                elif pathtoexists == False:
+                    wfile.write("from_ar.Seek(\"" + dirandstring + "\")" + "\n")
+                    wfile.write("from_ar.Write<string>(\"" + dirandwrite + "\")" + "\n\n")
 
-            wfile.write("from_ar.Seek(\"" + sstring + "\")" + "\n")
-            wfile.write("from_ar.Write<string>(\"" + wstring + "\")" + "\n\n")
+                    wfile.write("from_ar.Seek(\"" + sstring + "\")" + "\n")
+                    wfile.write("from_ar.Write<string>(\"" + wstring + "\")" + "\n\n")
 print("Wrote imports and swaps!")
 
 
 newpath = os.path.basename(jsonpath).split(".")[0] + ".rd"
 rdpath = os.path.dirname(jsonpath) + "/" + newpath
 
-print(rdpath)
-
 wfile.close()
 
-with open(rdpath, "a") as appending:
-    appending.write("from_ar.Swap(to_ar)\n")
-    appending.flush()
+if pathtoexists == True and swapsarenull == True:
+    with open(rdpath, "a") as appending:
+        appending.write("from_ar.Swap(to_ar)\n")
+        appending.flush()
 
 print("We will now compile the plugin. Please note you need the SSPN.repl.exe in the same folder as the python file for this to work. Continue? Type yes or no.")
 
