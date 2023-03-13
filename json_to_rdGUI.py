0115c25d238d6073d9eb2b57d6b2da7c93a423b7
Lightning <112514086+Lightning-isnt-a-dev@users.noreply.github.com>
2023-03-13 23:09:43 +0200

    Update json_to_rdGUI.py

--- json_to_rdGUI.py
+++ json_to_rdGUI.py
@@ -1,10 +1,18 @@
 import json, yaml
-import os, time
+import os, sys
 import tkinter, customtkinter
 import subprocess as sp
 import easygui
+from PIL import Image
 
-#installed with pip: easygui, customtkinter, pyyaml
+#installed with pip: easygui, customtkinter, pyyaml, pillow
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
 
 #def masterskeletalnull(replacestring):
     #if "Base" in os.path.dirname(replacestring) and ("Skeleton" in os.path.basename(replacestring) or "Fortnite_M_Avg_Player" in os.path.basename(replacestring)):
@@ -39,7 +47,7 @@ def seekwrite(pathtoexists, wfile, inpa, inpb):
             #wfile.write("from_ar.Seek(\"" + sstring + "\")" + "\n")
             #wfile.write("from_ar.Write<string>(\"" + wstring + "\")" + "\n\n")
 
-def json_to_rd(jsonpath):
+def json_to_rd(jsonpath, done):
     if os.path.exists(jsonpath):
         try:
             x = json.load(open(jsonpath, encoding='utf-8'))
@@ -120,18 +128,61 @@ def json_to_rd(jsonpath):
     done.configure(text="Done converting!", text_color="white")
     wfile.close()
 
-def rd_to_csp(rd):
+def rd_to_csp(rd, done, compile_frame):
     if os.path.exists(rd):
         if os.path.exists("SSPN.Repl.exe"):
             sp.Popen('SSPN.repl.exe "%%localappdata%%/saturn/plugins"  "%s"' % (rd), shell=True)
             done.configure(text="Done compiling!", text_color="white")
         elif not os.path.exists("SSPN.Repl.exe"):
-            done.configure(text="Compiler not found! Downloading compiler...", text_color="red")
-            sp.Popen('powershell -Command "wget -O SSPN.Repl.exe https://cdn.discordapp.com/attachments/1078667568082071623/1079002439866388480/SSPN.Repl.exe"', shell=True)
-            done.configure(text="Compiler downloaded! Please press the button again.", text_color="white")
+            done.configure(text="Compiler not found!", text_color="red")
+            downloadcompiler = customtkinter.CTkButton(compile_frame, text="Download Compiler (give it time)", command=lambda: downcompiler(done))
+            downloadcompiler.pack(padx=5, pady=5, side="left")          
     else:
         done.configure(text="Please convert your json first.", text_color="red")
 
+def defcolortheme(inp):
+    if inp == "Blue":
+        customtkinter.set_default_color_theme("blue")
+    elif inp == "Green":
+        customtkinter.set_default_color_theme("green")
+    elif inp == "Dark Blue":
+        customtkinter.set_default_color_theme("dark-blue")
+    info2.configure(text="Return to homepage to set your theme to " + inp.lower() + ".")
+
+
+def downcompiler(done):
+    getcompiler = sp.Popen('powershell -Command "wget -O SSPN.Repl.exe https://cdn.discordapp.com/attachments/1078667568082071623/1079002439866388480/SSPN.Repl.exe"', shell=True)
+    getcompiler.wait()
+    done.configure(text="Compiler downloaded!", text_color="white")
+
+def settingspage(settings, info_frame, convert_frame, compile_frame):
+    settings.destroy()
+    info_frame.destroy()
+    convert_frame.destroy()
+    compile_frame.destroy()
+
+    global returntomain
+    returnicon=customtkinter.CTkImage(Image.open(resource_path("icons/return.png")), size=(32, 32))
+    returntomain = customtkinter.CTkButton(app, image=returnicon, text="", fg_color="gray", width=32, height=32, command=mainpage)
+    returntomain.pack(padx=5, pady=5, anchor="nw")
+
+    global dropdownframe
+    dropdownframe = customtkinter.CTkFrame(app, fg_color="transparent")
+    dropdownframe.pack()
+
+    option = tkinter.StringVar()
+    dropdown = customtkinter.CTkOptionMenu(dropdownframe, values=["System", "Dark", "Light"], variable=option, command=lambda option : customtkinter.set_appearance_mode(option))
+    option.set("Choose Appearance Color")
+    dropdown.pack(padx=5, pady=5, side="left")
+
+    option2 = tkinter.StringVar()
+    dropdown2 = customtkinter.CTkOptionMenu(dropdownframe, values=["Blue", "Dark Blue", "Green"], variable=option2, command=lambda option2 : defcolortheme(option2))
+    option2.set("Choose Color Theme")
+    dropdown2.pack(padx=5, pady=5, side="right")
+
+    global info2
+    info2 = customtkinter.CTkLabel(app, text="")
+    info2.pack()
 
 #listoftests BrilliantBomber.json default_axe_to_stellar_axe.json Selene_To_The_Rogue_LAROI_ELECTRIFIED.json Sludgehammer_To_Candy_Axe.json BoogieDownToGetGriddy.json
 customtkinter.set_appearance_mode("dark")
@@ -139,25 +190,47 @@ customtkinter.set_default_color_theme("dark-blue")
 
 app = customtkinter.CTk()
 app.geometry("500x250")
-app.title("JSON to RD converter")
+app.title("Json to RD converter")
 
-title = customtkinter.CTkLabel(app, text="Input the path to your json:")
-title.pack(padx=10, pady=10)
+def mainpage():
+    try:
+        returntomain.destroy()
+        dropdownframe.destroy()
+        info2.destroy()
+    except:
+        pass
+    settingscog=customtkinter.CTkImage(Image.open(resource_path("icons/settings.png")), size=(32, 32))
+    settings = customtkinter.CTkButton(app, image=settingscog, text="", fg_color="transparent", width=32, height=32, command=lambda: settingspage(settings, info_frame, convert_frame, compile_frame))
+    settings.pack(padx=5, pady=5, anchor="ne")
 
-pathfielddata = tkinter.StringVar()
-pathfield = customtkinter.CTkEntry(app, width=350, height=40, textvariable=pathfielddata)
-pathfield.pack()
+    info_frame = customtkinter.CTkFrame(app, fg_color="transparent")
+    info_frame.pack()
 
-done = customtkinter.CTkLabel(app, text="")
-done.pack()
+    title = customtkinter.CTkLabel(info_frame, text="Input the path to your json:")
+    title.pack(padx=6, pady=9)
 
-jsonpathbutton = customtkinter.CTkButton(app, text="Select a JSON file", command=lambda : pathfielddata.set(easygui.fileopenbox()))
-jsonpathbutton.pack(padx=5, pady=5)
+    pathfielddata = tkinter.StringVar()
+    pathfield = customtkinter.CTkEntry(info_frame, width=350, height=40, textvariable=pathfielddata)
+    pathfield.pack()
 
-converttord = customtkinter.CTkButton(app, text="Convert to RD", command=lambda : json_to_rd(pathfielddata.get()))
-converttord.pack(padx=5, pady=5)
+    done = customtkinter.CTkLabel(info_frame, text="")
+    done.pack()
 
-converttocsp = customtkinter.CTkButton(app, text="Compile", command=lambda : rd_to_csp(os.path.basename(pathfielddata.get()).split(".")[0] + ".rd"))
-converttocsp.pack(padx=5, pady=5)
+    convert_frame = customtkinter.CTkFrame(app, fg_color="transparent")
+    convert_frame.pack()
 
-app.mainloop()
+    compile_frame = customtkinter.CTkFrame(app, fg_color="transparent")
+    compile_frame.pack()
+
+    jsonpathbutton = customtkinter.CTkButton(convert_frame, text="Select a JSON file", command=lambda : pathfielddata.set(easygui.fileopenbox()))
+    jsonpathbutton.pack(padx=5, pady=5, side='left')
+
+    converttord = customtkinter.CTkButton(convert_frame, text="Convert to RD", command=lambda : json_to_rd(pathfielddata.get(), done))
+    converttord.pack(padx=5, pady=5, side='left')
+
+    converttocsp = customtkinter.CTkButton(compile_frame, text="Compile", command=lambda : rd_to_csp(os.path.basename(pathfielddata.get()).split(".")[0] + ".rd", done, compile_frame))
+    converttocsp.pack(padx=5, pady=5, side='left')
+
+    app.mainloop()
+
+mainpage()
